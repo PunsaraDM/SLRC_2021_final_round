e11e7883af8450acb842c27891d1501b1f9d622b
Punsara Mahawela <pmahawela@gmail.com>
2021-10-30 13:11:06 +0530

    UPD - robot is moving

--- FR_sample_testing_arena/controllers/ARM_controller/ARM_controller.cpp
+++ FR_sample_testing_arena/controllers/ARM_controller/ARM_controller.cpp
@@ -15,6 +15,9 @@ char motor_names[4][20] = {"base_motor","vertical_motor","gripper_left_motor","g
 PositionSensor *linear_encoders[4];
 char linear_encoder_names[4][24] = {"base_motor_pos","vertical_motor_pos","gripper_left_motor_pos","gripper_right_motor_pos"};
 
+Motor *wheels[2];
+char wheels_names[2][15] = {"motor_left", "motor_right"};
+
 void init_motors(){
   for (int i=0; i < 4; i++){
     linear_motors[i] = robot->getMotor(motor_names[i]);
@@ -25,6 +28,13 @@ void init_motors(){
     linear_encoders[i] = robot->getPositionSensor(linear_encoder_names[i]);
     linear_encoders[i]->enable(TIME_STEP);
   }
+  
+  for (int i = 0; i < 2; i++) {
+  wheels[i] = robot->getMotor(wheels_names[i]);
+  wheels[i]->setPosition(INFINITY);
+  wheels[i]->setVelocity(0.0);
+  }
+
 }
 
 
@@ -40,7 +50,7 @@ void passive_wait(int motor_index, double target){
     effective = linear_encoders[motor_index]->getValue();
     //cout<<effective<<endl;
     dif = fabs(effective-target);             //check here again
-    cout<<"____"<<dif<<endl;
+    //cout<<"____"<<dif<<endl;
   } while (dif > DELTA);
 }
 
@@ -57,7 +67,7 @@ void passive_thread_wait(double targetLeft, double targetRight)
       effectiveLeft = linear_encoders[2]->getValue();
       effectiveRight = linear_encoders[3]->getValue();
       dif = min(fabs(effectiveLeft - targetLeft), fabs(effectiveRight - targetRight));
-      cout<<dif<<endl;
+      //cout<<dif<<endl;
     } while (dif > DELTA);
 }
 
@@ -88,11 +98,21 @@ void arm_grab_box(double targetLeft, double targetRight){
 
 
 int main(int argc, char **argv) {
+  int flag = 1;
   init_motors();
   while (robot->step(timeStep) != -1) {
+    if (flag==1){
     arm_base_move(0.04);
+    flag = 0;
+    }
     arm_grab_box(0.06,0.06);
     arm_vertical_move(0.05);
+    //arm_base_move(0.00);
+   for (int i = 0; i < 2; i++) {
+      wheels[i] = robot->getMotor(wheels_names[i]);
+      wheels[i]->setVelocity(5.0);
+    }   
+    
     //arm_vertical_move(0);
     //arm_base_move(0);
     //arm_grab_box(0.05,0.05);
